38ddb761d729194609ea109bcfba258f8c6bc2b9
shawnsaldanha79 <shawnsaldanha27816279@gmail.com>
2025-02-10 15:26:03 +0530

    create client

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  console.log("hello");
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline"> Hello world! </h1>
+    </div>
+  );
+}
