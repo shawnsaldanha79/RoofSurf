a84b457dc080216001d6c18058c751d6f5e008f6
shawnsaldanha79 <shawnsaldanha27816279@gmail.com>
2025-02-11 12:50:50 +0530

    create sign up page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-            <Link to='/'>
+            <Link to={'/'}>
             <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
                 <span className='text-slate-500'>Sahand</span>
                 <span className='text-slate-700'>Estate</span>
@@ -19,13 +19,13 @@ export default function Header() {
                 <FaSearch className='text-slate-500'/>
             </form>
             <ul className='flex gap-4'>
-                <Link to="/">
+                <Link to={"/"}>
                     <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
                 </Link>
-                <Link to="/about">
+                <Link to={"/about"}>
                     <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
                 </Link>
-                <Link to="/sign-in">
+                <Link to={"/sign-in"}>
                     <li className='text-slate-700 hover:underline'>Sign In</li>
                 </Link>
             </ul>
