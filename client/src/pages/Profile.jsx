ff6af1d2b8c0fde13b661bf1b875d5f8417fe1d8
shawnsaldanha79 <shawnsaldanha27816279@gmail.com>
2025-02-13 23:09:47 +0530

    add delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux"
 import { useEffect, useRef, useState } from "react"
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
 import { app } from "../firebase"
-import { updateUserStart, updateUserSuccess, updateUserFailure } from "../redux/user/userSlice"
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from "../redux/user/userSlice"
 import { useDispatch } from "react-redux"
 
 export default function Profile() {
@@ -64,6 +64,23 @@ export default function Profile() {
       dispatch(updateUserFailure(err.message))
     }
   }
+  const handleDeleteUser = async() => {
+    try {
+      dispatch(deleteUserStart())
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      })
+      const data = await res.json()
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message))
+        return
+      }
+      dispatch(deleteUserSuccess(data))
+    } catch (err) {
+      dispatch(deleteUserFailure(err.message))
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -90,7 +107,7 @@ export default function Profile() {
         hover:opacity-95 disabled:opacity-80">{ loading ? 'loading...' : 'Update' }</button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer ">Delete account</span>
+        <span onClick={ handleDeleteUser } className="text-red-700 cursor-pointer ">Delete account</span>
         <span className="text-red-700 cursor-pointer ">Sign out</span>
       </div>
       <p className="text-red-700 mt-5">{ error ? error : "" }</p>
