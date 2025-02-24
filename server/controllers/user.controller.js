982654ed240a6afd7e50130fcddbab1ec3a33326
shawnsaldanha79 <shawnsaldanha27816279@gmail.com>
2025-02-24 19:47:00 +0530

    complete show user listings functionality

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -49,7 +49,7 @@ export const getUserListings = async(req, res, next) => {
             const listings = await Listing.find({ userRef: req.params.id })
             res.status(200).json(listings)
         } catch (err) {
-            next(err)
+            next(err) 
         }
     } else {
         return next(errorHandler(401, 'You can only view your own listings!'))
