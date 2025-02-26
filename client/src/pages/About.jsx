706ba98ff297cba288e9d434ebf3caf7a8ea5bf5
shawnsaldanha79 <shawnsaldanha27816279@gmail.com>
2025-02-26 13:33:05 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Sahand Estate</h1>
+      <p className='mb-4 text-slate-700'>
+        At Sahand Real Estate, we are a team of dedicated professionals who are passionate about helping people find their dream homes.
+        With years of experience in the industry, we have the knowledge and expertise to guide you through the process of buying or selling a property.
+        Our agents are highly trained and committed to providing exceptional customer service.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        We are dedicated to helping you achieve your real estate goals and making the process as smooth and stress-free as possible.
+        Contact us today to learn more about how we can help you.
+      </p>
+    </div>
   )
 }
